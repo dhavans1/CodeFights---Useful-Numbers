9d6d18c4f5ddd3e2428f4cb305a458304660fdc9
Dhavan Shekarappa <dhavans1@umbc.edu>
2017-08-14 10:14:35 -0400

    Solution V1.0

---
+++ soln.py
@@ -0,0 +1,39 @@
+def usefulNumbers(k):
+#     Approach 1 : Time consuming
+#     count = 0
+#     if k%2 == 1:
+#         for i in range(k):
+#             if i%2 == 1:
+#                 continue
+#             if k+i == k|i:
+#                 count += 1
+#     else:
+#         for i in range(k):
+#             if k+i == k|i:
+#                 count += 1
+                
+#     return count
+
+#Approach 2 : Time consuming
+#     count = 0
+#     res = []
+#     for i in range(k):
+#         if k+i == k|i:
+#             count += 1
+#             res.append(i)
+#             
+#     print(k, bin(k)[2:],"\t", len(res), res)
+# #     print(res)
+#     return count
+
+#Approach 3
+    return pow(2, list(bin(k)[2:]).count('0'))
+
+def main():
+    k = 20
+    print(usefulNumbers(k))
+# 
+#     for i in range(10):
+#         usefulNumbers(i)
+    
+main()
